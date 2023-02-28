8a54d17c3931407d58fee90065ef9dc2977fc3e3
bandhavi <bandhavi.bendi@coupa.com>
2023-02-27 19:16:09 -0800

    passwordGenerator

---
+++ assets/script.js
@@ -0,0 +1,100 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//prompt for password lenght which returns length
+function showPromptForLength(){
+  var passwordLength = prompt("How many characters do you want your password to be? (min: 8, max: 128)");
+  const input = document.createElement("input");
+  input.type = "input";
+  
+  return passwordLength; 
+}
+
+//function to add a prompt for password Criteria for lowerCase
+function showPromptForLowerCase(){
+  const confirmed = window.confirm('Enter your password Criteria- do you want to include lowercase?');
+  //console.log(confirmed);
+  return confirmed;
+}
+
+//function to add a prompt for password Criteria for upperCase
+function showPromptForUpperCase(){
+  const confirmed = window.confirm('Enter your password Criteria- do you to include upperCase?');
+  //console.log(confirmed);
+  return confirmed;
+}
+
+//function to add a prompt for password Criteria for numerics
+function showPromptForNumerics(){
+  const confirmed = window.confirm('Enter your password Criteria- do you to include numerics?');
+  //console.log(confirmed);
+  return confirmed;
+}
+
+//function to add a prompt for password Criteria for specialcharacters
+function showPromptForSpecialCharacters(){
+  const confirmed = window.confirm('Enter your password Criteria- do you to include Specialcharacters?');
+  //console.log(confirmed);
+  return confirmed;
+}
+
+
+//function to generate password based user password criteria
+function generatePassword(){
+  var password = "";
+  //var passwordCriteria = prompt(" Please choose the password criteria");
+ var passwordLength = showPromptForLength();
+ var numeric = showPromptForNumerics();
+ var lowerCase = showPromptForLowerCase();
+ var upperCase = showPromptForUpperCase();
+ var specialCharacters = showPromptForSpecialCharacters();
+
+
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Please enter a password length between 8 and 128 characters.");
+  return;
+}
+
+ if (!lowerCase && !upperCase && !numeric && !includeSpecial) {
+  alert("Please select at least one type of character to include in the password.");
+    return;
+ }
+ var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+ var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var numericChars = "0123456789";
+ var specialChars = "!@#$%^&*";
+
+ var chars = "";
+ if (lowerCase) {
+   chars += lowercaseChars;
+ }
+ if (upperCase) {
+   chars += uppercaseChars;
+ }
+ if (numeric) {
+   chars += numericChars;
+ }
+ if (specialCharacters) {
+   chars += specialChars;
+ }
+
+ for (var i = 0; i < passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
+  alert("Your new password is: " + password);
+
+  return password;
+}
+
+
